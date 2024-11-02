3ce5fdba9123a3375d3660963adb69b147cbe111
manmadeharp <minibasher1090@gmail.com>
2024-11-02 11:41:08 +0000

    updates

--- Implementations/MetropolisHastings/RandomWalkMetropolisHastings.py
+++ Implementations/MetropolisHastings/RandomWalkMetropolisHastings.py
@@ -7,24 +7,24 @@ import scipy as sp
 
 
 def simple_model(a, b, c, d, e):
-    f = a*(b - c) - d*e
+    f = a * (b - c) - d * e
     return f
+
 
 class Forward_Model:
     def __init__(self, model: Callable):
         self.model = model
         self.param_names = list(inspect.signature(model).parameters.keys())
 
+
 class Parameter:
     def __init__(self, value: NDArray[np.float64], prior: Callable, scale):
         self.initial_value = value
         self.prior = prior
-        
 
 
 class RandomWalkMetropolisHastings:
     def __init__(self, prior, scale):
-
 
         # For proposals
         self.beta = scale
@@ -32,7 +32,7 @@ class RandomWalkMetropolisHastings:
         if initial_value == None:
             self.chain = np.zeros((0, params.size))
         else:
-            self.chain = np.atleast_2d(initial_value) # 2d for [ [initial_theta] ]
+            self.chain = np.atleast_2d(initial_value)  # 2d for [ [initial_theta] ]
 
     def prior():
 
@@ -40,3 +40,5 @@ class RandomWalkMetropolisHastings:
 
     def likelihood():
         return
+
+    # Random CHange
